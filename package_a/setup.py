50796759c17b37160650201c02247b3286ac3cc0
William Richard <wrichard@indigoag.com>
2021-11-12 10:51:54 -0500

    Ran `poetry update` and `poetry install` - click was not added

--- package_a/setup.py
+++ package_a/setup.py
@@ -5,12 +5,13 @@ from setuptools import find_namespace_packages, setup
 setup(
     name="a",
     description="Terraform Automation CLI for IndigoAg",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_namespace_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
         "boto3>=1.18.7,<2.0.0",
+        "click"
     ],
     extras_require={"dev": ['moto']},
     entry_points="""
