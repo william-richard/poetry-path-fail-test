cb020dc9f7574aa55bcfadb63561767bd0c26404
William Richard <wrichard@indigoag.com>
2021-11-12 10:00:59 -0500

    Initial commit

---
+++ package_a/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+Terraform Automation CLI for IndigoAg
+"""
+
+from setuptools import find_namespace_packages, setup  # type: ignore
+
+setup(
+    name="a",
+    description="Terraform Automation CLI for IndigoAg",
+    version="0.1.0",
+    packages=find_namespace_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=[
+        "boto3>=1.18.7,<2.0.0",
+    ],
+    extras_require={"dev": ['moto']},
+    entry_points="""
+        [console_scripts]
+        pkg_a=a.main:main
+        """,
+)
